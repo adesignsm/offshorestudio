dff5cafbf6e5fcc96ba0539dfc9af839edd4236d
Akash Mulye <akashmulye97@gmail.com>
2023-09-12 13:53:04 -0400

    added env to gitignore

--- src/Components/NewsLetter/index.jsx
+++ src/Components/NewsLetter/index.jsx
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./index.css";
 
+const apiKey = process.env.REACT_APP_MAILCHIMP_API_KEY;
+const audienceId = process.env.REACT_APP_MAILCHIMP_AUDIENCE_ID;
+
+console.log(apiKey)
+
 const NewsLetter = () => {
     const [email, setEmail] = useState("");
 
@@ -10,7 +15,7 @@ const NewsLetter = () => {
     
         try {
           const response = await axios.post(
-            'https://us9.api.mailchimp.com/3.0/lists/59b40def54/members',
+            `https://us9.api.mailchimp.com/3.0/lists/${audienceId}/members`,
             {
               email_address: email,
               status: 'subscribed',
@@ -18,7 +23,7 @@ const NewsLetter = () => {
             {
               headers: {
                 'Content-Type': 'application/json',
-                Authorization: '4cb23385b90f619480c20604f6e35668-us9',
+                Authorization: `${apiKey}`,
               },
             }
           );
