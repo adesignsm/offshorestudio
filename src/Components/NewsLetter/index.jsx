3a002ecb01dd7ce405aae772b8034b4e806211af
Akash Mulye <akashmulye97@gmail.com>
2023-09-12 14:43:58 -0400

    fixed mailchimp form

--- src/Components/NewsLetter/index.jsx
+++ src/Components/NewsLetter/index.jsx
@@ -1,47 +1,15 @@
 import React, { useState } from "react";
-import axios from "axios";
+import MailchimpSubscribe from "react-mailchimp-subscribe";
 import "./index.css";
 
-const apiKey = process.env.REACT_APP_MAILCHIMP_API_KEY;
-const audienceId = process.env.REACT_APP_MAILCHIMP_AUDIENCE_ID;
-
-console.log(apiKey)
+const mailchimpUrl = process.env.REACT_APP_MAILCHIMP_URL;
 
 const NewsLetter = () => {
-    const [email, setEmail] = useState("");
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-    
-        try {
-          const response = await axios.post(
-            `https://us9.api.mailchimp.com/3.0/lists/${audienceId}/members`,
-            {
-              email_address: email,
-              status: 'subscribed',
-            },
-            {
-              headers: {
-                'Content-Type': 'application/json',
-                Authorization: `${apiKey}`,
-              },
-            }
-          );
-    
-          console.log(response.data);
-        } catch (error) {
-          console.error(error);
-        }
-    };
 
     return (
         <>
-            <div>
-                <h2>Subscribe to Our Newsletter</h2>
-                <form onSubmit={handleSubmit}>
-                    <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    <button type="submit">Subscribe</button>
-                </form>
+            <div id="newsletter-form">
+                <MailchimpSubscribe url={mailchimpUrl} />
             </div>
         </>
     )
